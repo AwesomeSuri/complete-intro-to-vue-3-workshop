2a6e011b7c29d1140088a7f478e3a51cb802b8ac
AwesomeSuri <akbar.suriaganda@gmail.com>
2023-12-21 15:23:43 +0100

    move to vue project

---
+++ sandbox/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data: () => ({
+    message: 'Hello it works',
+    listOfNumbers: [1, 2, 3, 4, 5, 6],
+    count: 0,
+    incrementAmount: 1
+  }),
+  computed: {
+    displayedTitle() {
+      if (this.count > 20) return 'Counter - Large'
+      return 'Counter'
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count += this.incrementAmount
+    }
+  }
+}
+</script>
+
+<template>
+  <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul>
+    <li v-for="(number, index) in listOfNumbers" :key="`number-${index}`">{{ number }}</li>
+  </ul>
+  <hr />
+  <h1>{{ displayedTitle }}</h1>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCount">Increment</button>
+  <div>
+    <label for="incrementAmount">Increment by: </label>
+    <input type="number" v-model="incrementAmount" />
+  </div>
+</template>
